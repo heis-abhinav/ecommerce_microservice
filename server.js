e50f4945ee50f26afe9f019eb8a95f3fa923f38a
Abhinav Sharma <abhinav.sharma@instantsys.com>
2024-06-30 09:44:06 +0530

    bug fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
