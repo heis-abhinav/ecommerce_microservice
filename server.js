bc254effb005756f129d4a27f0ad3fca0e1febae
Abhinav Sharma <abhinav.sharma@instantsys.com>
2024-06-30 10:23:47 +0530

    routes updated

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ mongoose.connect(process.env.MONGO_URI)
 const registerRoute = require('./auth/register');
 const loginRoute = require('./auth/login');
 const authMiddleware = require('./middleware/authMiddleware');
+const createProductRoute = requier('./product/createProduct');
+const getProductsRoute = require('./product/getProducts');
+const updateProductRoute = require('./product/updateProduct');
+const deleteProductRoute = require('./product/deleteProduct');
+const getProductRoute = require('./product/getProduct');
+const createOrderRoute = require('./order/createOrder');
+const getOrdersRoute = require('./order/getOrders');
+const getOrderRoute = require('./order/getOrder');
+const updateOrderRoute = require('./order/updateOrder');
+const deleteOrderRoute = require('./order/deleteOrder'); 
 
 // Use routes
 app.use('/api/users', registerRoute);
@@ -27,6 +37,12 @@ app.use('/api/products', getProductsRoute);
 app.use('/api/products', updateProductRoute);
 app.use('/api/products', deleteProductRoute);
 app.use('/api/products', getProductRoute);
+app.use('/api/orders', createOrderRoute);
+app.use('/api/orders', getOrdersRoute);
+app.use('/api/orders', getOrderRoute);
+app.use('/api/orders', updateOrderRoute);
+app.use('/api/orders', deleteOrderRoute);
+
 // Protected route example
 app.get('/api/protected', authMiddleware, (req, res) => {
     res.send('This is a protected route');
